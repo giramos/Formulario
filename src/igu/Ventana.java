4cc3e344b620de36dc88604a608f1ef6c1b28bb3
giramos <UO202549@uniovi.es>
2024-04-07 13:34:54 +0200

    :two: refactorizando

--- src/igu/Ventana.java
+++ src/igu/Ventana.java
@@ -46,22 +46,6 @@ public class Ventana extends JFrame {
 	private JButton btnCancelar;
 
 	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Ventana frame = new Ventana();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
 	 * Create the frame.
 	 */
 	public Ventana() {
---
+++ src/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+import java.awt.EventQueue;
+
+import igu.Ventana;
+
+public class Main {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ventana frame = new Ventana();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
